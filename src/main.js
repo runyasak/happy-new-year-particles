521bdada938196efc606b40fcd5d1d0df49505f0
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2021-01-17 13:05:12 +0700

    feat: implement particles.vue3 on main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import Particles from 'particles.vue3';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+import './styles.css';
+
+createApp(App).use(Particles).mount('#app');
