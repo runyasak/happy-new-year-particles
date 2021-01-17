778dc9c91b3750b31a5e7cfc8547fe0d5ddc5a2f
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2021-01-17 13:09:53 +0700

    feat: implement Particles on App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,92 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <Particles
+      id="tsparticles"
+      class="snow-particle"
+      :options="{
+        background: {
+          color: {
+            value: '#18191A'
+          }
+        },
+        fpsLimit: 60,
+          interactivity: {
+          detectsOn: 'canvas',
+            events: {
+            onClick: {
+              enable: true,
+                mode: 'push'
+            },
+            onHover: {
+              enable: true,
+                mode: 'repulse'
+            },
+            resize: true
+          },
+          modes: {
+            bubble: {
+              distance: 400,
+                duration: 2,
+                  opacity: 0.8,
+                    size: 40
+            },
+            push: {
+              quantity: 4
+            },
+            repulse: {
+              distance: 200,
+                duration: 0.4
+            }
+          }
+        },
+        particles: {
+          color: {
+            value: '#ffffff'
+          },
+          links: {
+            color: '#ffffff',
+              distance: 150,
+                enable: true,
+                  opacity: 0.5,
+                    width: 1
+          },
+          collisions: {
+            enable: true
+          },
+          move: {
+            direction: 'none',
+              enable: true,
+                outMode: 'bounce',
+                  random: false,
+                    speed: 6,
+                      straight: false
+          },
+          number: {
+            density: {
+              enable: true,
+                value_area: 800
+            },
+            value: 80
+          },
+          opacity: {
+            value: 0.5
+          },
+          shape: {
+            type: 'circle'
+          },
+          size: {
+            random: true,
+              value: 5
+          }
+        },
+        detectRetina: true
+      }"
+    ></Particles>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.snow-particle {
+  height: 100vh;
 }
 </style>
